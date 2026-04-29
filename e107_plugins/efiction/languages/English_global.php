0ea0e4068e7d832f6954b6069749410c8010fb97
Jimako <janamicek@gmail.com>
2026-04-29 18:14:14 +0200

    preview fix

---
+++ e107_plugins/efiction/languages/English_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
++----------------------------------------------------------------------+
+*/
+ 
+
+return [
+    'LAN_PLUGIN_EFICTION' => "eFiction",
+    'LAN_PLUGIN_EFICTION_NAME' => "eFiction Plugin",
+    'LAN_PLUGIN_EFICTION_DESCRIPTION' => "",
+
+];
